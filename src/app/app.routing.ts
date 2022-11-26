564acea3c381af1db9874b504d5ecc8d983560f5
Marcin Garbarczyk <marcingarbarczyk93@gmail.com>
2022-11-26 01:26:10 +0100

    Clean project, SCSS compiler, routing

---
+++ src/app/app.routing.ts
@@ -0,0 +1,11 @@
+import {Routes} from "@angular/router";
+import {AppComponent} from "./app.component";
+
+
+export const AppRoutes: Routes = [
+  {
+    path: "test",
+    component: AppComponent
+  }
+]
+
